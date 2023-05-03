3a1d2a0fd2c55967c1b927703a7c0a4ad0d90ca7
ststchochocho <130932417+ststchochocho@users.noreply.github.com>
2023-05-03 23:11:25 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,6 @@ const toDoList = document.querySelector("#todo-list");
 const todoLane = document.getElementById("todo-lane");
 
 const TODOS_KEY = "todos";
-
 let toDos = [];
 
 function saveToDos() {
@@ -15,73 +14,79 @@ function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
-  saveToDos(); // deleteToDo에 안적으면 todo지워도 계속 남음(save 됨)
+  saveToDos();
 }
 
-function paintToDo(newTodo) {
-  const li = document.createElement("li");
-  li.id = newTodo.id;
-  const span = document.createElement("span");
-  span.innerText = newTodo.text;
-  span.setAttribute("draggble", "true");
-  span.draggable = true; 
+function handleDragStart(event) {
+  event.target.classList.add("is-dragging");
+}
 
-  span.addEventListener("dragstart", () => {
-    span.classList.add("is-dragging");
-  });
+function handleDragEnd(event) {
+  event.target.classList.remove("is-dragging");
+}
 
-  span.addEventListener("dragend", () => {
-    span.classList.remove("is-dragging");
-  });
- 
+function handleDrop(event) {
+  event.preventDefault();
+  const swimlane = event.target.closest(".swim_lane");
+  const li = document.querySelector(".is-dragging").parentElement;
+  swimlane.appendChild(li);
+}
 
-  
+function handleDragOver(event) {
+  event.preventDefault();
+  event.currentTarget.style.backgroundColor = "lightgray";
+}
 
-  function handleDrop(event) {
-    event.preventDefault();
-   
-    const swimlane = event.target.closest(".swim_lane");
-    swimlane.appendChild(li);
- 
-  }
-  
-  
-  function handleDragOver(event) {
-    event.preventDefault();
-    event.currentTarget.style.backgroundColor = "lightgray";
-  }
-  
-  function handleDragLeave(event) {
-    event.currentTarget.style.backgroundColor = "";
-  }
-  
-  const swimLanes = document.querySelectorAll(".swim_lane");
+function handleDragLeave(event) {
+  event.currentTarget.style.backgroundColor = "";
+}
+
+function addSwimLaneListeners(swimLanes) {
   swimLanes.forEach((lane) => {
     lane.addEventListener("drop", handleDrop);
     lane.addEventListener("dragover", handleDragOver);
     lane.addEventListener("dragleave", handleDragLeave);
-  }); 
+  });
+}
 
-  /* span.addEventListener("dragstart", handleDragStart);
-  span.addEventListener("dragend", handleDragEnd); */
+function createToDoElement(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  span.draggable = true;
+  span.addEventListener("dragstart", handleDragStart);
+  span.addEventListener("dragend", handleDragEnd);
+  
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
+  
   li.appendChild(span);
   li.appendChild(button);
+  
+  return li;
+}
+
+function paintToDo(newTodo) {
+  const li = createToDoElement(newTodo);
   toDoList.appendChild(li);
 
-  
+  const swimLanes = document.querySelectorAll(".swim_lane");
+  addSwimLaneListeners(swimLanes);
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+
   const newTodoObj = {
     text: newTodo,
     id: Date.now(),
   };
+
   toDos.push(newTodoObj);
   paintToDo(newTodoObj);
   saveToDos();
@@ -89,17 +94,7 @@ function handleToDoSubmit(event) {
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-/* function handleDragStart(event) {
-  event.dataTransfer.setData("text/plain", event.target.id);
-  event.currentTarget.style.backgroundColor = "yellow";
-} */
-/* 
-function handleDragEnd(event) {
-  event.currentTarget.style.backgroundColor = "";
-} */
-
 const savedToDos = localStorage.getItem(TODOS_KEY);
-
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
